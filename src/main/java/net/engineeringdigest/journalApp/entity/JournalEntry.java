d4070eb4da4678f61c043fa1c1c99a7165561579
Srivamshi Alle <alle.srivamshinani@gmail.com>
2024-10-21 17:21:04 +0530

    First Push

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,32 @@
+package net.engineeringdigest.journalApp.entity;
+
+public class JournalEntry {
+
+    private long id;
+    private String title;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {//POJO class plain old java object
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
