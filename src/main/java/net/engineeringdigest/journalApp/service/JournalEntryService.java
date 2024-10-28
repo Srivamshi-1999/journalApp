c779cb3e14d124a102823ce9dfd1445409943f5a
Srivamshi Alle <alle.srivamshinani@gmail.com>
2024-10-28 21:28:53 +0530

    Atlas connectivity

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,12 +20,19 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
     
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDate.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveUser(user);
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDate.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            //user.setUserName(null);
+            userService.saveUser(user);
+        }catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("An error occured while saving the entry",e);
+        }
     }
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
